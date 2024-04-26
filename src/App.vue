375b95e083c91b063054759b19a25bac65d32e9c
radiance-nt <1808154289@qq.com>
2024-04-26 16:54:32 +0800

    init markdown

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <button @click="fetchMarkdown">Render Markdown</button>
+    <div class="markdown-container">
+      <div v-for="(item, index) in markdownList" :key="index" class="markdown-box">
+        <div v-html="markdownToHtml(item)"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import { marked } from 'marked';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      markdown: '',
+      markdownList: []
+    };
+  },
+  methods: {
+    markdownToHtml(markdown) {
+      return marked(markdown);
+    },
+    fetchMarkdown() {
+      axios.get('http://localhost:5000/api/get_content')
+        .then(response => {
+          this.markdownList.push(response.data);
+        })
+        .catch(error => {
+          console.error('Error fetching markdown:', error);
+        });
+    }
   }
 }
 </script>
@@ -22,5 +46,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.markdown-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.markdown-box {
+  min-width: 600px;
+  background-color: #f0f0f0;
+  padding: 20px;
+  margin-bottom: 20px;
+  text-align: left;
+  border-radius: 10px;
 }
 </style>
